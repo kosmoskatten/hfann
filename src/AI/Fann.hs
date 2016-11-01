8501a554e92d87e235a8c9896deaf2f43e30d532
Patrik Sandahl <patrik.sandahl@gmail.com>
2016-11-01 13:22:36 +0100

    Removed some doc tags (better readability ATM).

--- src/AI/Fann.hs
+++ src/AI/Fann.hs
@@ -49,13 +49,7 @@ data Fann = Fann
 -- When running the network, the bias nodes always emits 1.
 --
 -- Resources allocated using this function is released using 'destroy'.
-createStandard'3L :: Int
-                     -- ^ Number of neurons in the input layer.
-                  -> Int
-                     -- ^ Number of neurons in the hidden layer.
-                  -> Int
-                     -- ^ Number of neurons in the output layer.
-                  -> IO Fann
+createStandard'3L :: Int -> Int -> Int -> IO Fann
 createStandard'3L input hidden output =
     Fann <$> Glue.createStandard'3L (fromIntegral input)
                                     (fromIntegral hidden)
@@ -63,9 +57,7 @@ createStandard'3L input hidden output =
 
 -- | Constructs a backpropagation neural network from a configuration
 -- file, which has been saved by 'save'.
-createFromFile :: FilePath
-                  -- ^ Path to the network to load.
-               -> IO Fann
+createFromFile :: FilePath -> IO Fann
 createFromFile file =
     withCString file $ \file' ->
         Fann <$> Glue.createFromFile file'
@@ -76,93 +68,55 @@ createFromFile file =
 -- network and enables 'createFromFile' to create an exact copy of
 -- the neural network and all of the parameters associated
 -- with the neural network.
-save :: Fann
-        -- ^ The instance to be saved.
-     -> FilePath
-        -- ^ Filepath to where to save configuration.
-     -> IO Bool
+save :: Fann -> FilePath -> IO Bool
 save fann file =
     withCString file $ \file' -> do
         CInt r <- Glue.save (fannRec fann) file'
         return (r /= 0)
 
 -- | Destroys the entire network, properly freeing all the associated memory.
-destroy :: Fann
-           -- ^ The instance to be destroyed.
-        -> IO ()
+destroy :: Fann -> IO ()
 destroy = Glue.destroy . fannRec
 
 -- | Will run input through the neural network, returning an array
 -- of outputs, the number of which being equal to the number of neurons
 -- in the output layer.
-run :: Fann
-       -- ^ The instance to run.
-    -> Vec.Vector Float
-       -- ^ Input data to the network.
-    -> IO (Vec.Vector Float)
+run :: Fann -> Vec.Vector Float -> IO (Vec.Vector Float)
 run fann input =
     Vec.map (\(CFloat f) -> f) <$>
         (Glue.run (fannRec fann) $ Vec.map CFloat input)
 
 -- | Get the number of input neurons.
-numInput :: Fann
-             -- ^ The instance to be queried.
-         -> Int
+numInput :: Fann -> Int
 numInput = fromIntegral . Glue.numInput . fannRec
 
 -- | Get the number of output neurons.
-numOutput :: Fann
-             -- ^ The instance to be queried.
-          -> Int
+numOutput :: Fann -> Int
 numOutput = fromIntegral . Glue.numOutput . fannRec
 
 -- | Get the learning rate (default 0.7).
-learningRate :: Fann
-                -- ^ The instance to be queried.
-             -> Float
+learningRate :: Fann -> Float
 learningRate = fromCFloat . Glue.learningRate . fannRec
 
 -- | Set the learning rate.
-setLearningRate :: Fann
-                   -- ^ The instance to be updated.
-                -> Float
-                   -- ^ The new learning rate.
-                -> IO ()
+setLearningRate :: Fann -> Float -> IO ()
 setLearningRate fann rate =
     Glue.setLearningRate (fannRec fann) (CFloat rate)
 
 -- | Set the activation function for all of the hidden layers.
-setActivationFunctionHidden :: Fann
-                               -- ^ The instance to be updated.
-                            -> ActivationFunction
-                               -- ^ The given 'ActivationFunction'.
-                            -> IO ()
+setActivationFunctionHidden :: Fann -> ActivationFunction -> IO ()
 setActivationFunctionHidden fann activation = do
     let actVal = fromIntegral $ activationToInt activation
     Glue.setActivationFunctionHidden (fannRec fann) actVal
 
 -- | Set the activation function for all of the output layer.
-setActivationFunctionOutput :: Fann
-                               -- ^ The instance to be updated.
-                            -> ActivationFunction
-                               -- ^ The given 'ActivationFunction'.
-                            -> IO ()
+setActivationFunctionOutput :: Fann -> ActivationFunction -> IO ()
 setActivationFunctionOutput fann activation = do
     let actVal = fromIntegral $ activationToInt activation
     Glue.setActivationFunctionOutput (fannRec fann) actVal
 
 -- | Train the ANN with a dataset provided in the given file.
-trainOnFile :: Fann
-               -- ^ The instance to be trained.
-            -> FilePath
-               -- ^ File with training dataset.
-            -> Int
-               -- ^ Max number of epochs.
-            -> Int
-               -- ^ Number of epochs between reports.
-            -> Float
-               -- ^ The desired error.
-            -> IO ()
+trainOnFile :: Fann -> FilePath -> Int -> Int -> Float -> IO ()
 trainOnFile fann file epochs epochsPerReport desiredError =
     withCString file $ \file' ->
         Glue.trainOnFile (fannRec fann) file' (fromIntegral epochs)
